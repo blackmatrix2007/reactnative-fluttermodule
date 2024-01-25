af7adb0ec325616b9b7b07e73454177b5b9f6aab
DuongHoangSon <sondhoang@gmail.com>
2024-01-25 14:02:32 +0700

    fix: update

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_module/testsmartcapture.dart';
-import 'ext/template_method_channel.dart';
 
 class NavigationService {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 }
 
 void main() {
-  extMain();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
@@ -26,7 +23,7 @@ class _MyPageState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const SmartMyHomePage(),
+      home: Container(),
       navigatorKey: NavigationService.navigatorKey,
     );
   }
